847c144b6dc3c46c6dde8b89ebf01e3546b28408
FrancimarAlexandre <francimaralexandredt@gmil.com>
2024-06-13 12:16:24 -0300

    imagem de fundo

---
+++ game/main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk  # importando Pillow
+
+# criando a tela principal
+root = Tk()
+
+# coletando o tamanho da tela
+largura = root.winfo_screenwidth()
+altura = root.winfo_screenheight()
+
+class app():
+    def __init__(self):
+        self.root = root
+        self.configTela()
+        root.mainloop()
+
+    def configTela(self):
+        self.root.title("Game Card YU-GI-OH")
+        self.root.geometry(f"{largura}x{altura}")
+        self.root.resizable(False, False)
+
+        # carregando e redimensionando a imagem de fundo
+        imagem = Image.open("img/yugioh_background.jpg")
+        imagem = imagem.resize((largura, altura))
+        self.imagem = ImageTk.PhotoImage(imagem)
+
+        LabelImage = Label(self.root, image=self.imagem)
+        LabelImage.place(relx=0, rely=0, relheight=1.0, relwidth=1.0)
+
+if __name__ == "__main__":
+    app()
